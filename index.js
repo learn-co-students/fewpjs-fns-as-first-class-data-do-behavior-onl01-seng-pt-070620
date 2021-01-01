9d93032122a35ef443041a60f30540fe49ef5f9c
khargr14 <kay_hargrove@yahoo.com>
2021-01-01 08:52:24 -0500

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  let hour = time.split(':');
+  if ( parseInt(hour[0]) < 12 ){
+    return "Good Morning";
+  } else if (parseInt(hour[0]) > 12 && parseInt(hour[0]) < 17) {
+    return "Good Afternoon";
+  } else if (parseInt(hour[0]) > 17) {
+    return "Good Evening";
+  }
+};
+function displayMessage(msg){
+  document.getElementById("greeting").innerText = msg;
+}
+
