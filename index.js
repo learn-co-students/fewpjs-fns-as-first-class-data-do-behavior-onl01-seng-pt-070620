da7c55b1a0d7833ac72bfb2281a4a6d45c51a4c5
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-01-03 18:35:02 -0800

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,18 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let timeInt = parseInt(timeString.split(':'), 10)
+  if (timeInt < 12) {
+    return 'Good Morning';
+  }
+  else if (timeInt > 17) {
+    return 'Good Evening';
+  }
+  return 'Good Afternoon';
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(greet) {
+  document.getElementById("greeting").innerText = greet;
+}
