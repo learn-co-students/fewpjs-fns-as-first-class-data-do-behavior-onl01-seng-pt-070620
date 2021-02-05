1cc5912041f9c479d3cf43108b377fd000fda25b
adcadams <andrewdcadams@gmail.com>
2021-02-05 16:05:02 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,18 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeStr) {
+  let timeInt = parseInt(timeStr,10)
+  if ( timeInt < 12) return "Good Morning"
+  if ( timeInt > 17) return "Good Evening"
+  return "Good Afternoon"
+
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(argString){
+  document.getElementById("greeting").innerText = argString
+
+}
+
